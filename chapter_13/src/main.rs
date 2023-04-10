8bd888712776c3c0f79ec46d3e5cffe77f1f7e00
GeeKaven <i@tawawa.moe>
2023-04-10 17:28:19 +0800

    chapter_13: iterator and closure

---
+++ chapter_13/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
+
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    println!("V2: {:?}",v2)
+}
