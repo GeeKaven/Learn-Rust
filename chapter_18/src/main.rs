3239e10dd5f34e1a2afc9dd46ea7515e21e95680
GeeKaven <i@tawawa.moe>
2023-04-11 10:00:36 +0800

    chapter_18: match

---
+++ chapter_18/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    // match
+    let x = Some(1);
+    let x = match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    };
+    println!("x: {}", x.unwrap());
+
+    // if let
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+
+    // while let
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
+
+    // for
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+
+    let (x, y, z) = (1, 2, 3);
+}
